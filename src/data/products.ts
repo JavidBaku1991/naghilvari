e0e03cac02dd2105ad7587d18892ddda2a173752
Javid Baku <cavid-de-baku91@mail.ru>
2025-07-29 16:53:13 +0400

    resizde

--- src/data/products.ts
+++ src/data/products.ts
@@ -19,7 +19,13 @@ import ipekyolu from '../images/categories/paintings/huseyn/ipekyolu.png';
 import kuleyeqarsi from '../images/categories/paintings/huseyn/kuleyeqarsi.png';
 import quslar from '../images/categories/paintings/huseyn/quslar.png';
 import skripkaliqiz from '../images/categories/paintings/huseyn/skripkaliqiz.png';
-
+import qizqalasi from '../images/categories/paintings/huseyn/qizqalasi.jpeg';
+import payizinQizilRengi from '../images/categories/paintings/huseyn/payizinqizilrengi.jpeg';
+import naturmort from '../images/categories/paintings/huseyn/naturmort.jpeg';
+import kafe from '../images/categories/paintings/huseyn/kafe.jpeg'
+import yumruBaliqlar from '../images/categories/paintings/huseyn/yumru baliqlar.jpeg';
+import sahIsmayil from '../images/categories/paintings/huseyn/sahismayil.jpeg';
+import cetirli from '../images/categories/paintings/huseyn/cetirli.jpeg';
 
 
 
@@ -131,6 +137,90 @@ const products: Product[] = [
     artist: "Hüseyn Hacıyev",
     year: 2023,
     dimensions: "73-67"
+  },
+  {
+    id: "1.2",
+    title: "Payıızın qızıl rəngi",
+    description: "qarışıq texnika",
+    price: 200,
+    imageUrl: payizinQizilRengi,
+    category: "paintings",
+    featured: true,
+    artist: "Hüseyn Hacıyev",
+    year: 2012,
+    dimensions: "36*52"
+  },
+  {
+    id: "1.3",
+    title: "Əsrlərdən boylanan qala",
+    description: "qrafika",
+    price: 500,
+    imageUrl: qizqalasi,
+    category: "paintings",
+    featured: true,
+    artist: "Hüseyn Hacıyev",
+    year: 2002,
+    dimensions: "40*52"
+  },
+  {
+    id: "1.4",
+    title: "Natürmort",
+    description: "akvarel, qələm , kağız",
+    price: 400,
+    imageUrl: naturmort,
+    category: "paintings",
+    featured: true,
+    artist: "Hüseyn Hacıyev",
+    year: 2014,
+    dimensions: "45*50"
+  },
+  {
+    id: "1.5",
+    title: "Kafe",
+    description: "Divar kağızı fakturalı yağlı boya",
+    price: 300,
+    imageUrl: kafe,
+    category: "paintings",
+    featured: true,
+    artist: "Hüseyn Hacıyev",
+    year: 2024,
+    dimensions: "48*65"
+  },
+  {
+    id: "1.6",
+    title: "Yumru balıqlar",
+    description: "",
+    price: 300,
+    imageUrl: yumruBaliqlar,
+    category: "paintings",
+    featured: true,
+    artist: "Hüseyn Hacıyev",
+    year: 2024,
+    dimensions: "48sm"
+  },
+  {
+    id: "1.7",
+    title: "Şah İsmayıl",
+    description: "miniatür",
+    price: 1200,
+    imageUrl: sahIsmayil,
+    category: "paintings",
+    featured: true,
+    artist: "Hüseyn Hacıyev",
+    year: 2017,
+    dimensions: "25*33"
+  },
+  {
+    id: "1.8",
+    title: "Çətirli qız",
+    description: "Miniatürlü qrafika",
+    price:0,
+    imageUrl: sahIsmayil,
+    category: "paintings",
+    featured: true,
+    artist: "Hüseyn Hacıyev",
+    year: 2004,
+    dimensions: "16*14"
   },
   {
     id: "2",
